f0ab20ba629ced6d9dd8f0a3b6a58e7eb3bbd9b5
Eric Young <eric@aceshome.com>
2011-09-06 00:08:59 +0000

    setting the utility paths

---
+++ tklpatch/newznabapp/overlay/home/nzb/bin/file_locations.sql
@@ -0,0 +1,4 @@
+insert into site (setting, value) values ('ffmpegpath', '/usr/local/bin/ffmpeg');
+insert into site (setting, value) values ('unrarpath', '/usr/bin/unrar');
+insert into site (setting, value) values ('lamepath', '/usr/local/bin/lame');
+insert into site (setting, value) values ('mediainfopath', '/usr/bin/mediainfo');
