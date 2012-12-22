cf9569603ec7c9a00df02abde6a459b1752c79a2
Eric Young <eric@aceshome.com>
2012-12-22 11:44:38 +0000

    moving temp to a private directory

--- tklpatch/newznabapp/overlay/home/nzb/bin/file_locations.sql
+++ tklpatch/newznabapp/overlay/home/nzb/bin/file_locations.sql
@@ -1,7 +1,7 @@
 UPDATE site SET value = '/usr/local/bin/ffmpeg' WHERE setting = 'ffmpegpath';
 UPDATE site SET value = '/usr/local/bin/lame' WHERE setting = 'lamepath';
 UPDATE site SET value = '/usr/bin/unrar' WHERE setting = 'unrarpath';
-UPDATE site SET value = '/tmp' WHERE setting = 'tmpunrarpath';
+UPDATE site SET value = '/home/nzb/tmp' WHERE setting = 'tmpunrarpath';
 UPDATE site SET value = '/usr/bin/mediainfo' WHERE setting = 'mediainfopath';
 UPDATE site SET value = '/' WHERE setting = 'style';
 
